946cf03db7671394e68adba3f065cfead2e04218
nio101 <nio101@outlook.com>
2021-06-05 10:46:38 +0200

    minor fixes

--- docker_stuff/docker_grafana/run_me.sh
+++ docker_stuff/docker_grafana/run_me.sh
@@ -1,4 +1,4 @@
 docker stop grafana
 docker rm grafana
 docker pull grafana/grafana
-docker run -d --name grafana -p 3000:3000 --restart unless-stopped --memory="500M" -v $HOME/docker_grafana/grafana_data:/var/lib/grafana -e "GF_AUTH_ANONYMOUS_ENABLED=true" grafana/grafana
+docker run -d --name grafana -p 3000:3000 --restart unless-stopped --memory="500M" -v $HOME/docker_grafana/custom.ini,/etc/grafana/grafana.ini -v $HOME/docker_grafana/grafana_data:/var/lib/grafana -e "GF_AUTH_ANONYMOUS_ENABLED=true" grafana/grafana
